70518a8f7b86893c980e1d706dd97b68f305bd43
heawon <okhw97@gmail.com>
2021-11-13 14:22:13 +0900

    Chore: add runtime.txt (python version)

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
-import flask
 from PIL import Image
+import os
 import io
 from predict import get_predict_result
+from flask import request
+import flask
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = {"success": False}
-    if flask.request.files.get("image"):
-        image = flask.request.files["image"].read()
+    if request.files.get("image"):
+        image = request.files["image"].read()
         image = Image.open(io.BytesIO(image))
         result = get_predict_result(image)
         data["success"] = True
@@ -18,5 +20,5 @@ def predict():
     return flask.jsonify(data),404
 
 
-if __name__ == '__main__':
+if __name__== "__main__":
     app.run()
