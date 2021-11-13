aef2343bd51075d51dbd80cae74e6835615c82dc
heawon <okhw97@gmail.com>
2021-11-13 16:25:28 +0900

    Chore: open-cv 라이브러리 제거

--- predict.py
+++ predict.py
@@ -1,18 +1,15 @@
-import cv2
 from labels import place_labels
 import numpy as np
 from tensorflow.keras.models import load_model
-
+from PIL import Image
 model = load_model('./model/model.h5')
 
 def preprocess_img(image):
     if image.mode != "RGB":
         image = image.convert("RGB")
-
-    image = np.array(image, dtype=np.float32)
-    image = cv2.resize(image, (224, 224))
-    image = np.expand_dims(image, 0)
-    image = image / 255
+    img = image.resize((224, 224), Image.ANTIALIAS)
+    image = np.expand_dims(img, 0)
+    image = image / 255.0
     return image
 
 def get_predict_result(image):
