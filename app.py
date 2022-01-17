8f9826d35d160afcdd3e08a764a0bbb82dea46c3
osandra <okhw97@gmail.com>
2022-01-17 17:36:02 +0900

    Feat: 장소 타입에 따른 필터 적용 및 소요 시간 로깅 추가

--- app.py
+++ app.py
@@ -4,26 +4,41 @@ import io
 import os
 from predict import get_predict_result
 from flask import request
+from flask import g
 import flask
+import logging
+import time
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = {"success": False}
+    location_type = request.args.get('type')
     if request.files.get("image"):
         image = request.files["image"].read()
         image = Image.open(io.BytesIO(image))
-        result = get_predict_result(image)
+        result = get_predict_result(image, location_type)
         data["success"] = True
         data["data"] = result
         return flask.jsonify(data), 200
     return flask.jsonify(data), 404
 
 
-@app.route("/test", methods=["GET"])
-def test():
-    return "ok"
+@app.before_request
+def before_request():
+    g.start = time.time()
+    app.logger.info(" Request: {}".format(request.full_path))
+
+
+@app.after_request
+def after_request(response):
+    execution_time = time.time() - g.start
+    app.logger.info(" Response: status_code = {}, time = {}".format(response.status_code, execution_time))
+    return response
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
--- predict.py
+++ predict.py
@@ -1,8 +1,10 @@
-from labels import place_labels
+from labels import *
 import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
+
 model = load_model('./model/model.h5')
+
 
 def preprocess_img(image):
     if image.mode != "RGB":
@@ -12,11 +14,24 @@ def preprocess_img(image):
     image = image / 255.0
     return image
 
-def get_predict_result(image):
-    result = []
+
+def get_predict_result(image, location_type):
     image = preprocess_img(image)
     features = model.predict(image)
-    top_numbers = (-features).argsort()[:3]
-    for index in range(3):
-        result.append(place_labels[top_numbers[0][index]][1])
+    sorted_similar_idx = (-features).argsort()[:3]
+    result = get_filtered_result(location_type, sorted_similar_idx)
+    return result
+
+
+def get_filtered_result(location_type, top_numbers):
+    result = []
+    index = 0
+    while len(result) < 3:
+        search_target = place_labels[top_numbers[0][index]]
+        if (location_type == "cafe" and not search_target[0] in cafes) or (
+                location_type == "attraction" and not search_target[0] in attractions):
+            index += 1
+            continue
+        result.append(search_target[1])
+        index += 1
     return result
